5393d56a79e62cb0d5f9ff258cdb70cd052df8e8
Fatma Moataz <115799042+FatmaMoataz@users.noreply.github.com>
2026-02-13 15:32:45 +0200

    Change 'UI/UX Developer' to 'UI/UX Designer'

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed(".typing", {
-    strings:["","Frontend Developer", "UI/UX Developer", "Web Developer"],
+    strings:["","Frontend Developer", "UI/UX Designer", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
